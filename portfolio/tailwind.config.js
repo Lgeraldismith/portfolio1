10bc2a824cf0fac661fbc8cd4b08c92a15509848
Lgeraldismith <Lgeraldismith@ufl.edu>
2023-10-17 17:18:47 -0400

    Commit with my updated information

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'aboutme': '#FEFAE0',
+      'experience': '#283618',
+      'leadership': '#BC6C25',
+      'projects': '#AF9F8C',
+      'contactus': '#C7C7BB',
+      'white': '#ffffff',
+      'textColor': '#001F5B'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
